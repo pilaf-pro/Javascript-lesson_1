0f816adc4570e3f287663d24923c5128bf928fd2
pilaf-pro <tuhnse183768@fpt.edu.vn>
2024-09-08 19:32:16 +0700

    Add main.js file

---
+++ main.js
@@ -0,0 +1,59 @@
+// // Goi ham thong bao
+// alert('Hi Javascript basic!');
+
+// // Khai bao bien
+// var fullName = 'Jack 5 cu';
+// var age = 26;
+
+// Goi ham thong bao
+// alert(fullName);
+// alert(age);
+
+// Ham built-in:
+
+// 1.Ham Console
+// console.log(fullName);
+// console.warn(fullName);
+// console.error(fullName);
+
+// 2.Ham Confirm
+// confirm('Xac nhan ban du tuoi?');
+
+// 3.Ham Prompt
+// prompt('Xac nhan ban du tuoi!');
+
+// 4.Ham Set timeout (Sau 1s thi thuc hien function)
+// setTimeout(function() {
+//     alert('Thong bao');
+// }, 1000)
+
+// 5.Ham Set interval
+// setInterval(function() {
+//     console.log('Day la log' + Math.random())
+// }, 1000)
+
+// Toan tu
+
+// 1.Arithmetic
+// var a = 1 + 2;
+// console.log(a);
+
+// 2.Assignment
+// var fullName = 'Jack 5 cu';
+
+// 3.Comparison
+// var a = 1;
+// var b = 2;
+
+// if( a <= b){
+//     alert('Dung');
+// }
+
+// 4.Logical
+// var a = 1;
+// var b = 2;
+
+// if(a > 0 || b > 0){
+//    alert('a & b lon hon 0');
+// }
+
