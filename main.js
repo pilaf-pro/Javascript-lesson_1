072f4803cd18520a30e35406a8d637fa6c7a2462
pilaf-pro <tuhnse183768@fpt.edu.vn>
2024-09-09 15:58:04 +0700

    update

--- main.js
+++ main.js
@@ -57,3 +57,75 @@
 //    alert('a & b lon hon 0');
 // }
 
+//Data type
+
+// 1.Symbol
+// var id = Symbol('id'); // unique
+// var id2 = Symbol('id'); // unique
+
+// 2.Function
+// var myFunction = function(){
+//     alert('Hi Jack 60 toi!');
+// }
+
+// myFunction();
+
+// 3.Object
+// const vehicleOne = {
+//     brand: 'Ford',
+//     model: 'Mustang',
+//     type: 'car',
+//     year: 2021, 
+//     color: 'red'
+//   }
+
+//   console.log(typeof vehicleOne);
+  
+//   myVehicle1(vehicleOne);
+  
+//   function myVehicle1(vehicle) {
+//     const message = 'My ' + vehicle.type + ' is a ' + vehicle.color + ' ' + vehicle.brand + ' ' + vehicle.model + '.';
+  
+//     document.getElementById("demo").innerHTML = message;
+//   }
+
+//   myVehicle(vehicleOne);
+  
+//   function myVehicle({type, color, brand, model}) {
+//     const message = 'My ' + type + ' is a ' + color + ' ' + brand + ' ' + model + '.';
+  
+//     document.getElementById("demo2").innerHTML = message;
+//   }
+
+//   function calculate(a, b) {
+//     const add = a + b;
+//     const subtract = a - b;
+//     const multiply = a * b;
+//     const divide = a / b;
+  
+//     return [add, subtract, multiply, divide];
+//   }
+  
+//   const [add, subtract, multiply, divide] = calculate(4, 7);
+  
+//   document.write("<p>Sum: " + add + "</p>");
+//   document.write("<p>Difference " + subtract + "</p>");
+//   document.write("<p>Product: " + multiply + "</p>");
+//   document.write("<p>Quotient " + divide + "</p>");
+
+//   console.log(typeof add);
+
+// Toan tu so sanh
+
+// 1. === so sanh data type va value
+// var a = 1;
+// var b = '1';
+
+// console.log(a == b); // true
+// console.log(a === b) // false
+// 2. !==
+// var a = 1;
+// var b = '1';
+
+// console.log(a != b); // false
+// console.log(a !== b); // true
